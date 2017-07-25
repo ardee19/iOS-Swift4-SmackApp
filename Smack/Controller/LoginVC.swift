90e7ad01646b185684edb2fb7e358bbf61fb5393
Roy Dimayuga <mr_nicelittleboy@yahoo.com>
2017-07-25 04:48:35 -0700

    Implement Login VC UI

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Super Awesome on 7/25/17.
+//  Copyright © 2017 Super Awesome. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Super Awesome on 7/25/17.
+//  Copyright © 2017 Super Awesome. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+struct SegueID {
+    static let toLogin = "toLogin"
+}
