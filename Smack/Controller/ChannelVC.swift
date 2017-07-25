153fb6cc2faf2b5c936db877e68434e4c84a69ea
Roy Dimayuga <mr_nicelittleboy@yahoo.com>
2017-07-25 03:28:48 -0700

    Implement SWRevealViewController

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,21 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Super Awesome on 7/25/17.
+//  Copyright © 2017 Super Awesome. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+    
+    
+
+}
---
+++ Smack/Smack-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Smack-Bridging-Header.h
+//  Smack
+//
+//  Created by Super Awesome on 7/25/17.
+//  Copyright © 2017 Super Awesome. All rights reserved.
+//
+
+#import "SWRevealViewController.h"
+
+/* Smack_Bridging_Header_h */
