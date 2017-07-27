c8aa97ba8bdb0feabaf4a9f821fac7709054622e
Roy Dimayuga <mr_nicelittleboy@yahoo.com>
2017-07-26 19:01:23 -0700

    Register User

---
+++ Smack/View/RoundedButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedButton.swift
+//  Smack
+//
+//  Created by Super Awesome on 7/26/17.
+//  Copyright © 2017 Super Awesome. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
